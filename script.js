e848600fd479952af4a3fed5e3c09c89e50d4ad5
183hardik <125795943+183hardik@users.noreply.github.com>
2023-04-05 18:08:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  async function callapi(){
+    let result = await fetch('https://api.ecelliitr.org/edc/community');
+    result = await result.json();
+    console.warn(result);
+    document.getElementById('userdata').innerHTML= result
+    .map((user)=>
+    `<tr>
+    <td><img src="${user.image}" height="80px"width="80px"></img></td>
+    <td><b><div class="name"margin-right="40px">${user.name}</div></b></td>
+    <td>${user.description}</td>
+    <td>${user.email}</td>
+    <td>${user.contact}</td>
+    <td><a href="${user.linkedin}"><i class="fa-brands fa-linkedin"></i></a><td>
+    </tr>`
+    ).join("");
+
+  }
+  callapi();
+  
